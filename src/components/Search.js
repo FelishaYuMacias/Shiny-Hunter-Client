0d00bfa69fffce59d5ae80ce884eabbddfee9088
tkmarsten <tkmarsten@gmail.com>
2022-12-06 14:38:38 -0800

    Fix bugs and file restructure

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,83 @@
-// import React from "react";
-// import { Form, Container } from 'react-bootstrap';
+import { useState } from 'react'
+import axios from 'axios'
 
-// export default function Search(props){
+const Search = () => {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("")
 
-//     return(
-//        <Container>
-//         <Form classname="mt-2">
-//             <Form.Row classname="align-item-center">
-//             <Col sm={10} classname="my-1">
-//             <Form.Control
-//             placeholder="Search for Pokemon" />
-//             </Form.Row>
-//             </Col>
-//         </Form>
-//        </Container>
-//     )
-// }
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      const res = await axios.get(url)
+      toArray.push(res.data)
+      setPokemonType(res.data.types[0].type.name)
+      setPokemonData(toArray)
+      console.log(res)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon()
+  }
+
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Enter Pokemon Name"
+          />
+        </label>
+      </form>
+      {pokemonData.map((data) => {
+        return (
+          <div className='container'>
+            <img src={data.sprites["front_shiny"]} />
+            <div className='divTable'>
+              <div className='divTableBody'>
+                <div className='divTableRow'>
+                  <div className='divTableCell'>Type</div>
+                  <div className='divTableCell'>{pokemonType}</div>
+                </div>
+                <div className='divTableRow'>
+                  <div className='divTableCell'>Form</div>
+                  <div className='divTableCell'>
+                    {" "}
+                    {Math.round(data.height * 3.9)} "
+                  </div>
+                </div>
+                <div className='divTableRow'>
+                  <div className='divTableCell'>Weight</div>
+                  <div className='divTableCell'>
+                    {" "}
+                    {Math.round(data.weight / 4.3)} lbs
+                  </div>
+                </div>
+                <div className='divTableRow'>
+                  <div className='divTableCell'>Number of Battle</div>
+                  <div className='divTableCell'>{data.game_indices.length}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      })
+      }
+    </>
+  )
+}
+
+export default Search
