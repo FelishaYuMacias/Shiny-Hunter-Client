9f63f9b5bcea10d5a0ed9bfa17092f5d2067adfc
tkmarsten <tkmarsten@gmail.com>
2022-12-07 10:33:05 -0800

    Add API functions

--- src/util/API.js
+++ src/util/API.js
@@ -1,13 +1,52 @@
+import axios from 'axios'
 const URL_PREFIX = 'https://localhost:3001' || 'https://herokuapp.com'
 
 const API = {
   login: (user) => {
-
+    return axios.post(`${URL_PREFIX}/api/users/login`, {
+      user
+    })
   },
   signup: (user) => {
-
+    return axios.post(`${URL_PREFIX}/api/users/signup`, {
+      user
+    })
   },
   getUserFromToken: (token) => {
-
+    return axios.get(`${URL_PREFIX}/api/users/getuserfromtoken`, {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+  },
+  getUserHunts: (userId) => {
+    return axios.get(`${URL_PREFIX}/api/hunts`, {
+      params: { __v: userId }
+    })
+  },
+  createHunt: (hunt, token) => {
+    return axios.post(`${URL_PREFIX}/api/hunts`, {
+      hunt,
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+  },
+  editHunt: (hunt, huntId, token) => {
+    return axios.put(`${URL_PREFIX}/api/hunts/${huntId}`, {
+      hunt,
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+  },
+  deleteHunt: (huntId, token) => {
+    return axios.delete(`${URL_PREFIX}/api/hunts/${huntId}`, {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
   }
 }
+
+export default API
