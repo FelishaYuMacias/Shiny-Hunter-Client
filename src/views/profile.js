80b3160b7f6374a2315a361001af153062f5ae49
James Pace <pacej2022@gmail.com>
2022-12-09 14:57:27 -0800

    update

--- src/views/profile.js
+++ src/views/profile.js
@@ -30,7 +30,7 @@ export default function Profile(props) {
         props.isLoggedIn?(
         <div className="Profile">
         <h1>Welcome {props.username}!</h1>
-        <Hunts user_Id={props.id} token={props.token}/>
+        <Hunts userId={props.id} token={props.token}/>
         </div>
         ):(
             <h1>Loading....</h1>
