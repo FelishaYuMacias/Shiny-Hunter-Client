bea355f4665d5a67655b5dc91b711ff3a41f32a5
Felisha <felisha.j.yu@gmail.com>
2022-12-08 16:09:08 -0800

    added hunts to user profile

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,44 +1,39 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from "react-router-dom"
+import React,{useEffect} from 'react'
+import Hunts from './Hunts'
 import API from "../util/API"
 
 export default function Profile(props) {
-  const navigate = useNavigate();
-  // useEffect(()=>{
-  //     if(!props.isLoggedIn){
-  //         navigate("/login")
-  //     }
-  // },[])
-  useEffect(() => {
-    const storedToken = localStorage.getItem("token")
-    if (storedToken) {
-      console.log(storedToken)
-      API.getUserFromToken(storedToken).then(data => {
-        if (data.user) {
-          console.log(data)
-          props.setToken(storedToken)
-          props.setIsLoggedIn(true)
-          props.setUserId(data.user.id)
+    useEffect(()=>{
+        const storedToken = localStorage.getItem("token")
+        if(storedToken){
+          console.log(storedToken)
+          API.getUserFromToken(storedToken).then(data=>{
+            if(data.user){
+              console.log(data)
+              props.setToken(storedToken)
+              props.setIsLoggedIn(true)
+              props.setUserId(data.user.id)
+            } else {
+                navigate("/login")
+            }
+          })
         } else {
+          console.log('no stored token')
           navigate("/login")
         }
-      })
-    } else {
-      console.log('no stored token')
-      navigate("/login")
-    }
-  }, [])
+      },[])
   return (
     <>
-      {
-        props.isLoggedIn ? (
-          <div className="Profile">
-            <h1>Welcome {props.username}!</h1>
-          </div>
-        ) : (
-          <h1>Loading....</h1>
+    {
+        props.isLoggedIn?(
+        <div className="Profile">
+        <h1>Welcome {props.username}!</h1>
+        <Hunts userId={props.id} token={props.token}/>
+        </div>
+        ):(
+            <h1>Loading....</h1>
         )
-      }
+    }
     </>
   )
 }
