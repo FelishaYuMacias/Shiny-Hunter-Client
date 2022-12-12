96a2312e6bcc2c9c9a4058e710c1d65188c39ceb
Felisha <felisha.j.yu@gmail.com>
2022-12-12 10:44:33 -0800

    added className

---
+++ src/views/Profile.js
@@ -0,0 +1,83 @@
+import './Profile.css';
+import React, { useEffect, useState } from 'react'
+import Card from '../components/Card';
+import axios from 'axios'
+const URL_PREFIX = 'https://shiny-hunter-server.herokuapp.com'
+
+export default function Profile(props) {
+  // console.log(props.userId)
+
+
+  const [user, setUser] = useState([])
+  const [userHunts, setUserhunts] = useState([])
+  const fetchUser = async () => {
+    const { data } = await axios.get(
+      `${URL_PREFIX}/api/users/${props.userId}`
+    );
+    const user = data;
+    setUser(user);
+    const userHunts = user.hunts;
+    setUserhunts(userHunts)
+    // console.log(user.hunts);
+    // console.log(userHunts)
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+
+  return (
+    <>
+      {
+        props.isLoggedIn ? (
+          <div className="Profile">
+
+            <h1>Welcome {user.username}!</h1>
+
+            <div>
+
+              <h2> Your Hunts:</h2>
+
+              <div>
+                {userHunts.map((hunts, index) => (
+                <div className= "nes-container">
+                  <ul className = "hunts" key={index}>
+                    <li>Method: {hunts.method}</li>
+                    <li>Counter: {hunts.counter}</li>
+                    <li>Phase: {hunts.phase}</li>
+                    <li>Pokemon: {hunts.pokemon.species}</li>
+                    <ul className = "hunts" key={index}>
+                    <Card pokemon={hunts.pokemon.species} />
+                    <li>Level: {hunts.pokemon.level}</li>
+                    <li>Form: {hunts.pokemon.form}</li>
+                    <li>Gender: {hunts.pokemon.gender}</li>
+                  </ul>
+                  </ul>
+                </div>
+                ))}
+              </div>
+              {/* <div>
+              <h2> Your Pokemon:</h2>
+              <div>
+                {userHunts.map((hunts, index) => (
+                  <ul className = "hunts" key={index}>
+                    <Card pokemon={hunts.pokemon.species} />
+                    <li>Species: {hunts.pokemon.species}</li>
+                    <li>Level: {hunts.pokemon.level}</li>
+                    <li>Form: {hunts.pokemon.form}</li>
+                    <li>Gender: {hunts.pokemon.gender}</li>
+                  </ul>
+                ))}
+              </div>
+            </div> */}
+
+          </div>
+          </div>
+        ) : (
+          <h1>Loading....</h1>
+        )
+      }
+    </>
+  )
+}
