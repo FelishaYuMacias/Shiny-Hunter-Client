48a5bf30acf09db1b02a4fe05f3c91d19be4f019
tkmarsten <tkmarsten@gmail.com>
2022-12-12 22:44:01 -0800

    Add persistant data

--- src/views/Huntform.js
+++ src/views/Huntform.js
@@ -1,25 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
 import API from '../util/API'
 
 const Huntform = (props) => {
-  const [method, setMethod] = useState('');
-  const [phase, setPhase] = useState('');
-  const [count, setCount] = useState(0);
-  const [pokemon, setPokemon] = useState(0);
+  const navigate = useNavigate();
 
-  const handleInputChange = (e) => {
+  const [pokemon, setPokemon] = useState(() => {
+    const hunt = JSON.parse(localStorage.getItem('currentHunt'))
+    if (hunt) {
+      return hunt.pokemon
+    } else {
+      return null
+    }
+  })
+  const [method, setMethod] = useState(() => {
+    const hunt = JSON.parse(localStorage.getItem('currentHunt'))
+    if (hunt) {
+      return hunt.method
+    } else {
+      return "Random encounter"
+    }
+  })
+  const [count, setCount] = useState(() => {
+    const hunt = JSON.parse(localStorage.getItem('currentHunt'))
+    if (hunt) {
+      return hunt.count
+    } else {
+      return 0
+    }
+  })
 
-    const { name, value } = e.target;
+  const handlePokemon = () => {
+    setPokemon(document.querySelector('#pokemonName').value)
+  }
 
+  const handleMethod = () => {
+    setMethod(document.querySelector('#huntMethod').value)
+  }
 
-    return method === 'method' ? setMethod(value) : setPhase(value);
-  };
+  const handleIncrease = () => {
+    setCount((count) => count + 1);
+  }
+  const handleDecrease = () => {
+    setCount((count) => count - 1);
+  }
+
+  const handleChange = () => {
+    setCount(count)
+  }
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    const userId = JSON.parse(localStorage.getItem('user')).id
-    const userToken = JSON.parse(localStorage.getItem('user')).token
+    const user = JSON.parse(localStorage.getItem('user'))
+    const userId = user.id
+    const userToken = user.token
 
     const pokemon = {
       species: document.querySelector('#pokemonName').value,
@@ -38,24 +73,21 @@ const Huntform = (props) => {
 
       console.log(hunt)
 
-      API.createHunt(hunt, userToken).then(res => {
-        if (res.ok) {
-          alert('woooooo')
-        }
-      })
+      API.createHunt(hunt, userToken)
+      navigate('/profile')
+      localStorage.removeItem('currentHunt')
     })
-  };
+  }
 
   useEffect(() => {
-    localStorage.setItem('myCount', count);
-  });
+    const currentHunt = {
+      pokemon: pokemon,
+      method: method,
+      count: count
+    }
 
-  const handleIncrease = () => {
-    setCount(count + 1);
-  };
-  const handleDecrease = () => {
-    setCount(count - 1);
-  };
+    localStorage.setItem('currentHunt', JSON.stringify(currentHunt));
+  })
 
   return (
     <div>
@@ -65,30 +97,27 @@ const Huntform = (props) => {
       <h2>Enter Hunt Below</h2>
       <form className="form">
         <div className="nes-field">
-          <input type="text" id="pokemonName" placeholder="Pokemon" className="nes-input" />
+          <input type="text" className="nes-input" id="pokemonName" placeholder="Pokemon" value={pokemon} onChange={handlePokemon} />
         </div>
         <div className="nes-select">
-          <select required id="huntMethod">
-            <option value="" disabled selected hidden>Select...</option>
-            <option value="0">Random encounter</option>
-            <option value="1">Soft Reset</option>
-            <option value="2">Eggs</option>
-            <option value="3">Trade</option>
-            <option value="4">Masuda</option>
-            <option value="5">Pokeradar</option>
-            <option value="6">Chain fishing</option>
-            <option value="7">Friend safari</option>
-            <option value="8">Horde</option>
-            <option value="9">Dexnav</option>
-            <option value="10">S.O.S.</option>
-            <option value="11">Wormhole</option>
+          <select required id="huntMethod" value={method} onChange={handleMethod}>
+            <option value="Random encounter">Random encounter</option>
+            <option value="Soft Reset">Soft Reset</option>
+            <option value="Eggs">Eggs</option>
+            <option value="Trade">Trade</option>
+            <option value="Masuda">Masuda</option>
+            <option value="Pokeradar">Pokeradar</option>
+            <option value="Chain fishing">Chain fishing</option>
+            <option value="Friend safari">Friend safari</option>
+            <option value="Horde">Horde</option>
+            <option value="Dexnav">Dexnav</option>
+            <option value="S.O.S.">S.O.S.</option>
+            <option value="Wormhole">Wormhole</option>
           </select>
         </div>
-        <input
-          name="Count"
-          placeholder={count}
-        />
-
+        <div className="nes-field">
+          <input type="number" className="nes-input" value={count} onChange={handleChange} />
+        </div>
         <button type="button" className="nes-btn is-primary" onClick={handleDecrease}>
           -
         </button>
