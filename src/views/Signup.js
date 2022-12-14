78a10eebf2d85153a1544b9b65fe8b399962e490
Jonnvoo <Jonathanvu2065@gmail.com>
2022-12-14 00:30:57 -0800

    trying to do form validation

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import API from '../util/API'
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from '@chakra-ui/react'
 
-const Login = (props) => {
-  const [signupUsername, setSignupUsername] = useState(null)
-  const [signupPassword, setSignupPassword] = useState(null)
 
+const Signup = (props) => {
+  const [signupUsername, setSignupUsername] = useState("")
+  const [signupPassword, setSignupPassword] = useState("")
+
+  const handleInputChange = (e) => setSignupUsername(e.target.value)
+
+  const isError = signupUsername === ""
   const navigate = useNavigate();
   useEffect(() => {
     if (props.isLoggedIn) {
@@ -45,27 +55,36 @@ const Login = (props) => {
   }
 
   return (
+
+
     <div>
 
-      <form onSubmit={handleSignup}>
+      <FormControl isInvalid={isError}
+      onSubmit={handleSignup}>
         <h3>Signup</h3>
         <input
           placeholder="Username"
           value={signupUsername}
-          onChange={e => setSignupUsername(e.target.value)}
-        />
+          onChange={handleInputChange}
+          />
         <input
           type="password"
           placeholder="Password"
           value={signupPassword}
-          onChange={e => setSignupPassword(e.target.value)}
-        />
-        <button>Signup!</button>
-      </form>
+          onChange={e => setSignupPassword(e.target.value)}/>
+          {!isError ? (
+        <FormHelperText>
+          Enter the email you'd like to receive the newsletter on.
+        </FormHelperText>
+      ) : (
+        <FormErrorMessage>Email is required.</FormErrorMessage>
+      )}
+        <button type="button" class="nes-btn is-error">Signup!</button>
+      </FormControl>
       <button className="link-btn" onClick={() => props.onFormSwitch('Login')}>Have a Account? Login here.</button>
 
     </div>
   )
 }
 
-export default Login
+export default Signup
