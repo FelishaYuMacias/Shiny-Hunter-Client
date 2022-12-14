bd72aded96f657ac20fd97ce3e83fc5cfa1d60bf
tkmarsten <tkmarsten@gmail.com>
2022-12-14 10:20:13 -0800

    Move data rendering to Card component

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -2,7 +2,7 @@ import './Profile.css';
 import React, { useEffect, useState } from 'react'
 import Card from '../components/Card';
 import API from '../util/API'
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 const Profile = (props) => {
   const navigate = useNavigate()
@@ -54,48 +54,25 @@ const Profile = (props) => {
     <>
       <h1>Welcome {user.username}!</h1>
       <h2> Your Hunts:</h2>
-      {
-        props.isLoggedIn ? (
-          <div>
-            <div>
-              {userHunts.map((hunts, index) => (
-                <div className='row'>
-                  <div className='column'>
-                    <div className="nes-container is-rounded">
-                      <div className='card'>
-                        <div className="nes-container is-rounded">
-                          <h3>{hunts.pokemon.species}
-                            <Card pokemon={hunts.pokemon.species} />
-                          </h3>
-                        </div>
-                      </div>
-                      <div className="text" key={index}>
-                        <h4>Method</h4>
-                        <p>{hunts.method}</p>
-                        <h4>Counter</h4>
-                        <p> {hunts.counter}</p>
-                        <h4>Phase:</h4>
-                        <p>{hunts.phase}</p>
-                        <h4>Level:</h4>
-                        <p> {hunts.pokemon.level}</p>
-                        <h4>Form:</h4>
-                        <p> {hunts.pokemon.form}</p>
-                        <h4>Gender:</h4>
-                        <p>{hunts.pokemon.gender}</p>
-                        <button id={hunts._id} type="button" className="nes-btn is-primary" onClick={handleUpdate}>Update
-                        </button>
-                        <button type="button" className="nes-btn is-error" data-buttonid={index} onClick={handleDelete}>Delete</button>
-                      </div>
-                    </div>
+      {props.isLoggedIn ? (
+        <>
+          {
+            userHunts.map((data, index) => (
+              <div className='row'>
+                <div className='column'>
+                  <div className="nes-container is-rounded">
+                    <Card hunt={data} key={index} />
+                    <button id={data._id} type="button" className="nes-btn is-primary" onClick={handleUpdate}>Update</button>
+                    <button type="button" className="nes-btn is-error" data-buttonid={index} onClick={handleDelete}>Delete</button>
                   </div>
                 </div>
-              )
-              )}
-            </div>
-          </div>
-        ) : (
-          <h1>Loading....</h1>
-        )
+              </div>
+            ))
+          }
+        </>
+      ) : (
+        <h1>Loading....</h1>
+      )
       }
     </>
   )
