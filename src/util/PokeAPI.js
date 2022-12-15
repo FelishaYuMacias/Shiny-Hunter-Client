c3f1bc2bc3d2bcb10838fa01eeaa6b388d2bc40e
tkmarsten <tkmarsten@gmail.com>
2022-12-14 17:16:48 -0800

    Update to search

--- src/util/PokeAPI.js
+++ src/util/PokeAPI.js
@@ -3,6 +3,9 @@ import axios from "axios"
 const URL = 'https://pokeapi.co/api/v2/pokemon'
 
 const PokeAPI = {
+  getPokemon: async (pokemon) => {
+    return await axios.get(`${URL}/${pokemon}`).then(res => res.data)
+  },
   getSprite: async (pokemon) => {
     return await axios.get(`${URL}/${pokemon}`).then(res => res.data.sprites.other.home.front_shiny)
   },
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,26 +3,18 @@ import Search from "../components/Search";
 const Home = () => {
   return (
     <div>
-<h1>Professor Oak's Poke Dex</h1>
-  <i className="nes-bulbasaur"></i>
-  <i className="nes-charmander"></i>
-  <i className="nes-squirtle"></i>
-      <section className="nes-container">
-  
-    <Search/>
-
-    <section class="message -right">
-      
-      <div class="nes-balloon from-right">
-        <p>Its a PokeDex search for your favorite Pokemon</p>
-      </div>
-      <i class="nes-bcrikko"></i>
-    </section>
-  </section>
-
-    
-    
-    </div>
+      <h1>Professor Oak's Poke Dex</h1>
+      <i className="nes-bulbasaur"></i>
+      <i className="nes-charmander"></i>
+      <i className="nes-squirtle"></i>
+      <section className="message -right">
+        <div className="nes-balloon from-right">
+          <p>Use the PokeDex to search for your favorite Pokemon!</p>
+        </div>
+        <i className="nes-bcrikko"></i>
+      </section>
+      <Search />
+    </div >
   )
 }
 
